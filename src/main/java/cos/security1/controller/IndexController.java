99089be5b9f254b1222ef814e883173667bbdd2a
fDevJc <fdevjc@gmail.com>
2022-05-13 15:33:13 +0900

    init commit

---
+++ src/main/java/cos/security1/controller/IndexController.java
@@ -0,0 +1,15 @@
+package cos.security1.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        log.info("indexController");
+        return "index";
+    }
+}
